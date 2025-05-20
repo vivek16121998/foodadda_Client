ae61f6d154af23761c4b8c5b60ac1cc8ce06bfc8
vivek16121998 <vivekkumargautam664@gmail.com>
2025-05-21 01:04:58 +0530

    backend url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,30 +1,31 @@
 import { environment as common } from './environment';
 
 
-const server="backend.foodadda.co.in"
+const server="ezuuoxnl4g.execute-api.ap-south-1.amazonaws.com"
 const portnumber="80"
+const applicationName="v1"
 
 export const environment = {
 
 
 
-    getRestaurants : "https://"+server+"/searchAPI/viewAllRestaurants",
-    registerUser:"https://"+server+"/UserAPI/userRegister",
-    getOperatingRestaurants: "https://"+server+"/AdminAPI/allRestaurants",
-    deleteRestaurant:"https://"+server+"/AdminAPI/deleteRestaurant/",
-    newRestaurants:"https://"+server+"/AdminAPI/newlyAddedRestaurants",
-    approveRestaurant:"https://"+server+"/AdminAPI/grantPermission/",
-    getRatingsBasedRestaurant:"https://"+server+"/AdminAPI/ratingsBasedRestaurant/",
-    loginUri : "https://"+server+"/UserAPI/userLogin",
-    getOrders:"https://"+server+"/orderAPI/viewAllOrders/",
-    bookTable:"https://"+server+"/booktableAPI/book/",
-    getRestaurantName:"https://"+server+"/orderAPI/getRestaurantName/",
-    placeOrder:"https://"+server+"/orderAPI/newOrder/",
-    getAreaRecommendations:"https://"+server+"/recommendationAPI/restaurantsByArea",
-    getAddressList:"https://"+server+"/UserAPI/getAddressList/",
-    addAddress:"https://"+server+"/UserAPI/addAddress/",
-    deleteAddress:"https://"+server+"/UserAPI/deleteAddress/",
-    updateAddress:"https://"+server+"/UserAPI/updateAddress/",
+    getRestaurants : "https://"+server+"/"+applicationName+"/searchAPI/viewAllRestaurants",
+    registerUser:"https://"+server+"/"+applicationName+"/UserAPI/userRegister",
+    getOperatingRestaurants: "https://"+server+"/"+applicationName+"/AdminAPI/allRestaurants",
+    deleteRestaurant:"https://"+server+"/"+applicationName+"/AdminAPI/deleteRestaurant/",
+    newRestaurants:"https://"+server+"/"+applicationName+"/AdminAPI/newlyAddedRestaurants",
+    approveRestaurant:"https://"+server+"/"+applicationName+"/AdminAPI/grantPermission/",
+    getRatingsBasedRestaurant:"https://"+server+"/"+applicationName+"/AdminAPI/ratingsBasedRestaurant/",
+    loginUri : "https://"+server+"/"+applicationName+"/UserAPI/userLogin",
+    getOrders:"https://"+server+"/"+applicationName+"/orderAPI/viewAllOrders/",
+    bookTable:"https://"+server+"/"+applicationName+"/booktableAPI/book/",
+    getRestaurantName:"https://"+server+"/"+applicationName+"/orderAPI/getRestaurantName/",
+    placeOrder:"https://"+server+"/"+applicationName+"/orderAPI/newOrder/",
+    getAreaRecommendations:"https://"+server+"/"+applicationName+"/recommendationAPI/restaurantsByArea",
+    getAddressList:"https://"+server+"/"+applicationName+"/UserAPI/getAddressList/",
+    addAddress:"https://"+server+"/"+applicationName+"/UserAPI/addAddress/",
+    deleteAddress:"https://"+server+"/"+applicationName+"/UserAPI/deleteAddress/",
+    updateAddress:"https://"+server+"/"+applicationName+"/UserAPI/updateAddress/",
 
 
  production: true
